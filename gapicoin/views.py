1fd8d257f6e445c2317cba38d8b840b958d02a93
Omer-remo <49786638+Omer-remo@users.noreply.github.com>
2019-06-13 14:29:01 +0300

    Https problem resolved

--- gapicoin/views.py
+++ gapicoin/views.py
@@ -212,7 +212,7 @@ def sendgapicoin(request):
             )
             newtrans.save()
             ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
-            geturl = "http://{}/api/vi/gettransaction/{}/".format(ip,newtrans.id)
+            geturl = "http://{}/api/v1/gettransaction/{}/".format(ip,newtrans.id)
             test = {"server":False,
             "sender":sender,
             "receiver":receiverwallet,
